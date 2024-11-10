a785b26d15583f26af1c96938ec8e47cab59455b
AbhishekValiyapurakkal <abhiv6160@gmail.com>
2024-11-11 00:25:04 +0530

    final commit

--- lib/Api/notification_access_token.dart
+++ lib/Api/notification_access_token.dart
@@ -6,13 +6,12 @@ class NotificationAccessToken {
   static String? _token;
 
   //to generate token only once for an app run
-  static Future<String?> get getToken async => _token ?? await _getAccessToken();
+  static Future<String?> get getToken async =>
+      _token ?? await _getAccessToken();
 
   // to get admin bearer token
   static Future<String?> _getAccessToken() async {
-    try {
-      
-    } catch (e) {
+    try {} catch (e) {
       log('$e');
       return null;
     }
